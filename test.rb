223934b4b84e54731bffae9acbb57e413a1805b9
intdnp <intndp@gmail.com>
2022-11-20 21:43:27 +0200

    wave + refactor

--- test.rb
+++ test.rb
@@ -1,3 +1,17 @@
-def accum(s)
-  s.split("").map.with_index { |x, i| x.upcase + x.downcase * i}.join("-")
+def wave(str)
+  result = [str.capitalize]
+  c = 1
+  while (str.delete(' ').size) != result.size 
+    result << str[0..c-1] + str[c].upcase + str[c+1..-1] if str[c] != ' ' 
+    c += 1
+  end
+  str == "" ? [] : result
 end
+
+def wave1(str)
+  (0...str.size).map{|c| str[0...c].downcase + str[c].upcase + str[c + 1..-1].downcase if str[c] != " "}.compact
+end
+
+
+p wave('HELLO')
+p wave1('HELLO') 
